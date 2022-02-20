e5c707bdfe5b02d5162e318196e283d9f416d244
Your Name <dinca.toni@gmail.com>
2022-02-20 15:11:56 +0200

    Redux installed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TransactionList from './components/TransactionList';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <TransactionList/>
     </>
   );
 }
---
+++ frontend/src/components/TransactionList.js
@@ -0,0 +1,8 @@
+const TransactionList = () =>{
+    return(
+        <>
+            <button>Register Transaction</button>
+        </>
+    )
+}
+export default TransactionList
