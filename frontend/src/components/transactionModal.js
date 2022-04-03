cb48e43e60471d4830048fce273aba41d4907599
Your Name <dinca.toni@gmail.com>
2022-04-03 17:09:02 +0300

    added delete transaction functionality

---
+++ frontend/src/components/transactionModal.js
@@ -0,0 +1,17 @@
+import { connect } from "react-redux"
+import { addTransaction } from "../actions/transactionsActions"
+
+const TransactionModal = () => {
+
+    state = {
+        modal: false,
+        name: ''
+    }
+
+    toggle = () => {
+        
+    }
+
+}
+
+export default connect(addTransaction)(TransactionModal)
